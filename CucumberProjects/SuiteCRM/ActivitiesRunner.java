df8cb0e1304156d8ed43c559e5b60dcebeb1e019
SureshBillakanti <75249403+SureshBillakanti@users.noreply.github.com>
2021-01-20 11:57:23 +0530

    Update ActivitiesRunner.java

--- CucumberProjects/SuiteCRM/ActivitiesRunner.java
+++ CucumberProjects/SuiteCRM/ActivitiesRunner.java
@@ -9,13 +9,13 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 features = "Features",
 glue = {"stepDefinitions"},
-tags = "@jobAlchemy",
+//tags = "@jobAlchemy",
 //tags = "@OrangeHRM",
-//tags = "@SuiteCRM",
+tags = "@SuiteCRM",
 monochrome = true,
-plugin = {"html:reports2/jobAlchemy_Test_Report1.html"}
+//plugin = {"html:reports2/jobAlchemy_Test_Report1.html"}
 //plugin = {"html:reports2/OrangeHRM_Test_Report1.html"}
-//plugin = {"html:reports2/SuiteCRM_Test_Report1.html"}
+plugin = {"html:reports2/SuiteCRM_Test_Report1.html"}
 
 )
 
